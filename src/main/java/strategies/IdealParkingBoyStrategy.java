a7e1df78ec4287461eb0b74dff676359a5b1f064
tuber <btu@thoughtworks.com>
2016-11-22 22:43:53 +0800

    [tuber] init project.

--- src/main/java/strategies/IdealParkingBoyStrategy.java
+++
@@ -1,9 +0,0 @@
-package strategies;
-
-import models.ParkingLot;
-
-import java.util.List;
-
-public interface IdealParkingBoyStrategy {
-    ParkingLot getIdealParkingLot(List<ParkingLot> parkingLots);
-}
