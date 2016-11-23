c9ac5014d8f74d26d6480d3f153d5ee4471b5d72
tuber <btu@thoughtworks.com>
2016-11-23 22:15:58 +0800

    [tuber] refactor SuperParkingBoy to extend Boy.

--- src/main/java/models/SuperParkingBoy.java
+++ src/main/java/models/SuperParkingBoy.java
@@ -1,31 +1,9 @@
 package models;
 
-import exceptions.FailToParkException;
-import exceptions.FailToPickException;
 import strategies.HighVacancyRateStrategy;
-import strategies.IdealParkingLotStrategy;
 
-import java.util.UUID;
-
-public class SuperParkingBoy {
-    private final ParkingLot[] parkingLots;
-    private final IdealParkingLotStrategy strategy;
-
+public class SuperParkingBoy extends Boy {
     public SuperParkingBoy(ParkingLot... parkingLots) {
-        this.parkingLots = parkingLots;
-        this.strategy = new HighVacancyRateStrategy();
-    }
-
-    public UUID park(Car car) throws FailToParkException {
-        return strategy.getIdealParkingLot(parkingLots).park(car);
-    }
-
-    public Car pick(UUID token) throws FailToPickException {
-        for (ParkingLot parkingLot : parkingLots) {
-            if (parkingLot.contains(token)) {
-                return parkingLot.pick(token);
-            }
-        }
-        throw new FailToPickException();
+        super(new HighVacancyRateStrategy(), parkingLots);
     }
 }
