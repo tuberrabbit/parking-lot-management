a44c265ea06270eb86bf8a630a847e10f48ad162
tuber <btu@thoughtworks.com>
2016-11-23 22:12:23 +0800

    [tuber] extract Boy.

--- src/main/java/models/ParkingBoy.java
+++ src/main/java/models/ParkingBoy.java
@@ -1,32 +1,9 @@
 package models;
 
-import exceptions.FailToParkException;
-import exceptions.FailToPickException;
-import strategies.IdealParkingLotStrategy;
 import strategies.InOrderStrategy;
 
-import java.util.UUID;
-
-public class ParkingBoy {
-
-    private final IdealParkingLotStrategy strategy;
-    private ParkingLot[] parkingLots;
-
+public class ParkingBoy extends Boy {
     public ParkingBoy(ParkingLot... parkingLots) {
-        this.parkingLots = parkingLots;
-        this.strategy = new InOrderStrategy();
-    }
-
-    public UUID park(Car car) throws FailToParkException {
-        return strategy.getIdealParkingLot(parkingLots).park(car);
-    }
-
-    public Car pick(UUID token) throws FailToPickException {
-        for (ParkingLot parkingLot : parkingLots) {
-            if (parkingLot.contains(token)) {
-                return parkingLot.pick(token);
-            }
-        }
-        throw new FailToPickException();
+        super(new InOrderStrategy(), parkingLots);
     }
 }
