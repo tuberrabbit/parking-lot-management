8f1c254ca45a5587a8dc3699b2bed9bf72679397
tuber <btu@thoughtworks.com>
2016-11-23 22:14:31 +0800

    [tuber] refactor SmartParkingBoy to extend Boy.

--- src/main/java/models/SmartParkingBoy.java
+++ src/main/java/models/SmartParkingBoy.java
@@ -1,31 +1,9 @@
 package models;
 
-import exceptions.FailToParkException;
-import exceptions.FailToPickException;
-import strategies.IdealParkingLotStrategy;
 import strategies.MoreEmptySpacesStrategy;
 
-import java.util.UUID;
-
-public class SmartParkingBoy {
-    private final ParkingLot[] parkingLots;
-    private final IdealParkingLotStrategy strategy;
-
+public class SmartParkingBoy extends Boy {
     public SmartParkingBoy(ParkingLot... parkingLots) {
-        this.parkingLots = parkingLots;
-        this.strategy = new MoreEmptySpacesStrategy();
-    }
-
-    public UUID park(Car car) throws FailToParkException {
-        return strategy.getIdealParkingLot(parkingLots).park(car);
-    }
-
-    public Car pick(UUID token) throws FailToPickException {
-        for (ParkingLot parkingLot : parkingLots) {
-            if (parkingLot.contains(token)) {
-                return parkingLot.pick(token);
-            }
-        }
-        throw new FailToPickException();
+        super(new MoreEmptySpacesStrategy(), parkingLots);
     }
 }
